692703e53bc64baff8fcaf7f820109891f183b73
NatureBoi <12373359+NatureBoi@users.noreply.github.com>
2019-01-05 21:56:59 -0500

    changes made to the nav, index.html, and skills.html some changes will be made to fix the javascript in the skills.html. Also added particle.js

--- html/js/script.js
+++ html/js/script.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+console.log("Hello");
+let counter = 0;
+
+let anim;
+
+let ht_img = document.querySelector(".html");
+let cs_img = document.querySelector(".css");
+let js_img = document.querySelector(".js");
+let ux_img = document.querySelector(".ux");
+
+let ht_ar = ["pug-logo.svg", "html.svg"];
+let cs_ar = ["sass.svg", "css.svg"];
+let js_ar = ["angular.svg", "react.svg", "js.png"];
+let ux_ar = ["ai.svg", "ps.svg"];
+
+function myfunc() {
+  anim = setInterval(nf, 1000);
+}
+
+function nf() {
+  setIcon(ht_img, ht_ar);
+  setIcon(cs_img, cs_ar);
+  setIcon(js_img, js_ar);
+  setIcon(ux_img, ux_ar);
+  counter++;
+}
+
+function setIcon(el, arr) {
+  if (counter >= arr.length) {
+    counter = 0;
+  }
+  el.src = `img/icons/${arr[counter]}`;
+}
