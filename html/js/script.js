352a626e43627682343ff97770efc8a63ef246f4
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-01-18 23:29:50 -0500

    fixed counter issue needed closure

--- html/js/script.js
+++ html/js/script.js
@@ -1,7 +1,4 @@
-console.log("Hello");
 let counter = 0;
-
-let anim;
 
 let ht_img = document.querySelector(".html");
 let cs_img = document.querySelector(".css");
@@ -14,14 +11,11 @@ let js_ar = ["angular.svg", "react.svg", "js.png"];
 let ux_ar = ["ai.svg", "ps.svg"];
 
 function myfunc() {
-  anim = setInterval(nf, 1000);
+  setInterval(js, 1000);
 }
 
-function nf() {
-  setIcon(ht_img, ht_ar);
-  setIcon(cs_img, cs_ar);
+function js() {
   setIcon(js_img, js_ar);
-  setIcon(ux_img, ux_ar);
   counter++;
 }
 
